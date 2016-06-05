e3717a62af6f2f7aa6a5a00c809ea3ac0343fefd
Joshua Motter <Josh_Motter@yahoo.com>
2016-06-05 15:14:59 -0700

    Openshift hosting change.

--- app.js
+++ app.js
@@ -59,9 +59,19 @@ app.get('/api/name', api.name);
 app.get('*', routes.default);
 
 /**
+ * Set values for Openshift hosting, and start the server
+ */
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+http.createServer(app).listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
+
+/**
  * Start Server
  */
 
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+//http.createServer(app).listen(app.get('port'), function () {
+//  console.log('Express server listening on port ' + app.get('port'));
+//});
